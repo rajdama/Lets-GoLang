44101e1f69015267240646ec8f22193e2dadb2ea
raj <rajdama1728@gmail.com>
2023-08-23 20:32:37 +0530

    typecasting

--- variables/variables.go
+++ variables/variables.go
@@ -15,7 +15,7 @@ var2 = 0.1
 var3 = false
 var4 = "hello"
 
-fmt.Printf("%v %f %v %q\n",var1, var2, var3, var4 )
+fmt.Printf("%v %f %v %q\n",var1, var2, var3, var4 ) // Using Printf needs format specifying
 
 //second way to use variables (recommended)
 
@@ -25,4 +25,16 @@ var7 := false
 var8 := "hello"
 
 fmt.Printf("%v %f %v %q\n",var5, var6, var7, var8 )
+
+//declaring two variables simultaneously
+
+var9, var10 := .2, "hello"
+fmt.Println(var9, var10) // Using Println does not needs format specifying
+
+//typecasting
+
+var11 := 5.7
+var12 := int(var11) //Float to Int
+fmt.Println(var12) 
+
 }
