9ef89905aad12b51c53b94ff8202a0c73764ddfa
raj <rajdama1729@gmail.com>
2023-08-25 11:52:22 +0530

    embedded structs

--- structs/main.go
+++ structs/main.go
@@ -22,30 +22,54 @@ type user struct {
 	number int
 }
 
+// Embedded struct (kinda inheritence)
+type dog struct {
+	name string
+	age int
+}
+
+type animal struct {
+	// "car" is embeddded, so the definition of a
+	// "truck" now also additionally contains all
+	// of the fields of the car struct
+	dog
+	legs int
+}
+
 
 
 func main() {
-	myCar := car{}		 //All values initialized to default value i.e 0
 
-	myCar.Make = "honda" //Assigning values
+	myCar := car{}		    //All values initialized to default value i.e 0
+
+	myCar.Make = "honda" 	//Assigning values
 	myCar.Height = 5
 
-	fmt.Println(myCar)  // Prints simple struct
+	fmt.Println(myCar)  	// Prints simple struct
 
 	msg := messageToSend{}
 	msg.sender.name = "X"
 	msg.recipient.name = "Y"
 
-	fmt.Println(msg) // Prints nested struct
+	fmt.Println(msg) 		// Prints nested struct
 
-	Element := struct {
+	student := struct {  	//Anonymous structs prevent you from re-using a struct definition you never intended to re-use
         name      string
         branch    string
     }{
         name:      "Raj",
         branch:    "CSE",
     }
-  
 
-	fmt.Println(Element) // Prints anonymus struct
+	fmt.Println(student) 	// Prints anonymus struct
+
+	myDog := animal{}
+
+	myDog.name = "Buzo"
+	myDog.age = 10
+	myDog.legs = 4
+
+	fmt.Println(myDog)
+
+
 }
