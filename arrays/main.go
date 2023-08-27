e8a261cf9245ba0520455bf9fdff08e5adf2e554
raj <rajdama1729@gmail.com>
2023-08-27 11:23:41 +0530

    arrays printing

--- arrays/main.go
+++ arrays/main.go
@@ -3,7 +3,12 @@ import "fmt"
 
 func main() {
 	
-	//Declaration of an array with int type and size 10
+	// Declaration of an array with int type and size 10
 	var arr1 [10] int
+	fmt.Println(arr1)
+
+	// Declaration and initiazlization at same time
+	arr2 := [6]int{2,3,4,5,6,7}
+	fmt.Println(arr2)
 
 }
