2be3bbf59471cded212255add829c7308b5c11f8
raj <rajdama1729@gmail.com>
2023-08-27 11:01:54 +0530

    for loop without condition

--- loops/main.go
+++ loops/main.go
@@ -2,7 +2,17 @@ package main
 import "fmt"
 
 func main(){
+
+	//For loop with condition
 	for i := 0; i < 10; i++ {
 		fmt.Println(i)
+	}
+
+	//For loop without condition (runs forever)
+	for i := 0; ; i++ {
+		fmt.Println(i)
+		if i==1000{
+			break
+		}
 	}
 }
