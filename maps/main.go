3741c5f5fe2a40bab080674fbe2e79ec8a949a95
raj <rajdama1729@gmail.com>
2023-08-28 11:07:53 +0530

    maps instantition

--- maps/main.go
+++ maps/main.go
@@ -4,9 +4,18 @@ import "fmt"
 
 func main() {
 
+	// Using make keyword to form a map from string to int
 	ages := make(map[string]int)
-	ages["jhon"] = 37
+
+	ages["John"] = 37
 	ages["Mary"] = 35
 
 	fmt.Println(ages)
+
+	// Defining and instantiating map at same time
+	ages2 := map[string]int{
+		"John": 37,
+		"Mary": 35,
+	}
+	fmt.Println(ages2)
 }
