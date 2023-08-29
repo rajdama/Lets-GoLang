122921e6aa42989b0b400d1f1ed2869a2cfe6d8b
raj <rajdama1729@gmail.com>
2023-08-29 12:42:29 +0530

    passing pointer in function

--- pointers/main.go
+++ pointers/main.go
@@ -1,6 +1,13 @@
 package main
 import "fmt"
 
+
+func add (x, y *int) int {
+	return *x + *y
+}
+
+
+
 func main()  {
 
 	x := 7
@@ -9,4 +16,9 @@ func main()  {
 
 	z := *y			   // dereference operator for getting actual x value
 	fmt.Println(z)
+
+	sum := add(&x,&z)	// passing address in a function
+	fmt.Println(sum)
+
+	
 }
