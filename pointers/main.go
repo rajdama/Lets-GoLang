6c51278aab1d0d0e94f74652f3415f7e75d191ab
raj <rajdama1729@gmail.com>
2023-08-29 12:35:06 +0530

    dereference operator

--- pointers/main.go
+++ pointers/main.go
@@ -5,5 +5,8 @@ func main()  {
 
 	x := 7
 	y := &x            // y contains memory address of x
-	fmt.Println(*y)
+	fmt.Println(y)
+
+	z := *y			   // dereference operator for getting actual x value
+	fmt.Println(z)
 }
