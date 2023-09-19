da630980174aa2b627d55d24d90d961ee017474b
raj <rajdama1729@gmail.com>
2023-09-19 20:12:03 +0530

    unbuffered channels

--- channels/main.go
+++ channels/main.go
@@ -3,13 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	userch := make(chan string) //buffered channel i.e capacity is only 1
+	userch1 := make(chan string) //buffered channel i.e capacity is only 1
 
 	go func() {
-		userch <- "raj" //blocking
+		userch1 <- "raj" //blocking
 	}()
 
-	user := <-userch
+	user := <-userch1
+
+	fmt.Println(user)
+
+	userch2 := make(chan string, 2) //unbuffred channel i.e capacity is more than 1
+
+	go func() {
+		userch2 <- "ram" //non-blocking
+	}()
+
+	user = <-userch2
 
 	fmt.Println(user)
 }
